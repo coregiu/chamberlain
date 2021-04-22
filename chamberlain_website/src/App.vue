a9287e4ff7106b718b96e7002913bface439c938
giu-giu <195412063@qq.com>
2021-04-22 22:14:58 +0800

    Fix bug of vue router.

--- chamberlain_website/src/App.vue
+++ chamberlain_website/src/App.vue
@@ -3,19 +3,19 @@
     <div id="top">
       <a href="#"><img src="./assets/regiu-logo.png" id="logo"/></a>
       <div>
-        <router-link to="/blog"><img src="./assets/svgs/blog.svg"/>博客</router-link>
+        <router-link to="/blog" class="menu"><img src="./assets/svgs/blog.svg"/>博客</router-link>
       </div>
       <div>
-        <router-link to="/notebook"><img src="./assets/svgs/notebook.svg"/>记事本</router-link>
+        <router-link to="/notebook" class="menu"><img src="./assets/svgs/notebook.svg"/>记事本</router-link>
       </div>
       <div class="group">
-        <img src="./assets/svgs/input.svg">收入管理
+        <img src="./assets/svgs/input.svg" class="subMenu">收入管理
         <ul>
           <li>
-            <router-link to="/inputs_details">收入明细</router-link>
+            <router-link to="/inputs_details" class="subMenu">收入明细</router-link>
           </li>
           <li>
-            <router-link to="/inputs_analysis">收入分析</router-link>
+            <router-link to="/inputs_analysis" class="subMenu">收入分析</router-link>
           </li>
         </ul>
       </div>
@@ -23,30 +23,30 @@
         <img src="./assets/svgs/admin.svg"/>系统管理
         <ul>
           <li>
-            <router-link to="/urmgmt">用户管理</router-link>
+            <router-link to="/urmgmt" class="subMenu">用户管理</router-link>
           </li>
           <li>
-            <router-link to="/syslog">操作日志</router-link>
+            <router-link to="/syslog" class="subMenu">操作日志</router-link>
           </li>
           <li>
-            <router-link to="/sysback">备份恢复</router-link>
+            <router-link to="/sysback" class="subMenu">备份恢复</router-link>
           </li>
         </ul>
       </div>
       <div>
-        <router-link to="/login"><img src="./assets/svgs/login.svg"/>登录</router-link>
+        <router-link to="/login" class="menu"><img src="./assets/svgs/login.svg"/>登录</router-link>
       </div>
       <div>
         <img src="./assets/svgs/accout.svg"/>个人信息
         <ul>
           <li>
-            <router-link to="/userinfo">个人信息</router-link>
+            <router-link to="/userinfo" class="subMenu">个人信息</router-link>
           </li>
           <li>
-            <router-link to="/updatepassword">修改密码</router-link>
+            <router-link to="/updatepassword" class="subMenu">修改密码</router-link>
           </li>
           <li>
-            <router-link to="/logout">🚀 退出系统</router-link>
+            <router-link to="/logout" class="subMenu">🚀 退出系统</router-link>
           </li>
         </ul>
       </div>
